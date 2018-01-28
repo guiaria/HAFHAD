278faf2bca97e0427e89c94310789b82cef0f62a
Unknown <gui.aria1@gmail.com>
2018-01-28 19:14:22 +0700

    Add

---
+++ Project_speech/Open_Close.py
@@ -0,0 +1,84 @@
+from pythainlp.tag import pos_tag
+from pythainlp.corpus import stopwords
+from pythainlp.tokenize import word_tokenize
+import speech_recognition as sr
+import json
+import csv
+
+
+
+def open_close():
+    
+    # obtain audio from the microphone
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
+
+    # recognize speech using Google Speech Recognition
+    try:
+         print("Google Speech Recognition thinks you said " + r.recognize_google(audio, language ="th-TH"))
+         word_recv = r.recognize_google(audio, language ="th-TH")
+    except sr.UnknownValueError:
+        print("Google Speech Recognition could not understand audio")
+    except sr.RequestError as e:
+        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+ 
+    
+    
+    e=word_tokenize(word_recv,engine='newmm')
+    print(e)
+    
+    open_light = []
+    close_light = []
+    stopwords1 = stopwords.words('thai')
+    stopwords2 = ['สิ','ดิ','หน่อย']
+
+    filter_word1 = e
+    for word in e:
+
+        if any(['เปิด' in word, 'ปิด' in word]):
+            continue
+        if word in stopwords2:
+            filter_word1.remove(word)
+
+    filter_word = [word1 for word1 in filter_word1 if word1 not in stopwords2]
+    pos_list = pos_tag(filter_word,engine='artagger')
+    print(filter_word)
+
+
+    if(filter_word[0] == "เปิดไฟ"):
+        open_light.append(filter_word[1])
+        if len(filter_word) > 2:
+            if(filter_word[2] == "เเละ" and filter_word[-1] != "เเละ"):
+                open_light.append(filter_word[3])
+  
+
+    if(filter_word[0] == "เปิด" and filter_word[1] == "ปลั๊ก"):
+        open_light.append(filter_word[2])
+        if len(filter_word) > 3:
+            if(filter_word[3] == "เเละ" and filter_word[-1] != "เเละ"):
+                open_light.append(filter_word[4])
+
+
+    if(filter_word[0] == "ปิดไฟ"):
+        close_light.append(filter_word[1])
+        if len(filter_word) > 2:
+            if(filter_word[2] == "เเละ" and filter_word[-1] != "เเละ"):
+                 close_light.append(filter_word[3])
+ 
+
+    if(filter_word[0] == "ปิด" and filter_word[1] == "ปลั๊ก"):
+        close_light.append(filter_word[2])
+        if len(filter_word) > 3:
+            if(filter_word[3] == "เเละ" and filter_word[-1] != "เเละ"):
+                close_light.append(filter_word[4])
+    with open('list.csv', 'wb') as csvfile:
+        wr = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(open_light)
+        wr.writerow(close_light)
+
+if __name__ == "__main__":
+    open_close()
+    print("Finished")
