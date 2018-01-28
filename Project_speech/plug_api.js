e042fe18a3a7f81b6399867f6c906dea19529cb7
guiaria <gui.aria1@gmail.com>
2018-01-28 20:42:19 +0700

    Delete plug_api.js

--- Project_speech/plug_api.js
+++
@@ -1,8 +0,0 @@
-const Hs100Api = require('/usr/local/lib/node_modules/hs100-api'); 
-const client = new Hs100Api.Client();
-const plug = client.getDevice({host: '192.168.1.7'}).then((device)=>{
-  device.getSysInfo().then(console.log);
-  device.setPowerState(true);
-});
-
-
